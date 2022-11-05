afc1893afc24dc6eb583452d2c3326926ba9fda1
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-11-05 01:17:27 -0700

    update

--- sort.h
+++ sort.h
@@ -26,5 +26,6 @@ typedef struct listint_s
 /* Sorting algoritms */
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
 
 #endif
