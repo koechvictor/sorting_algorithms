44f228c1468288645c1d17eb9170f0d714805d4c
victor kipkirui koech <kipkiruiv408@gmail.com>
2022-11-05 01:32:22 -0700

    update

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -34,3 +34,43 @@ void sort_alg(int *arr, int left, int right, size_t size)
 		sort_alg(arr, pivot + 1, right, size);
 	}
 }
+
+/**
+  * split - split array
+  * @arr: array
+  * @left: leftmost index
+  * @right: rightmost index
+  * @size: full array size
+  * Return: pivot index
+  */
+int split(int *arr, int left, int right, size_t size)
+{
+	int i, i2, pivot, tmp;
+
+	pivot = arr[right];
+	i = left;
+
+	for (i2 = left; i2 < right; i2++)
+	{
+		if (arr[i2] < pivot)
+		{
+			if (i != i2)
+			{
+				tmp = arr[i2];
+				arr[i2] = arr[i];
+				arr[i] = tmp;
+				print_array(arr, size);
+			}
+			i++;
+		}
+	}
+	if (arr[i] != arr[right])
+	{
+		tmp = arr[i];
+		arr[i] = arr[right];
+		arr[right] = tmp;
+		print_array(arr, size);
+	}
+
+	return (i);
+}
